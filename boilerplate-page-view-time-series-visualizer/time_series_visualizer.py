bccbf955335792fdf99ef32672b26f98b377e566
Hamza-Toulni <toulni.hamza@outlook.com>
2022-01-01 00:23:57 +0100

    portfolio

--- boilerplate-page-view-time-series-visualizer/time_series_visualizer.py
+++ boilerplate-page-view-time-series-visualizer/time_series_visualizer.py
@@ -7,12 +7,8 @@ register_matplotlib_converters()
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
 df = pd.read_csv('fcc-forum-pageviews.csv', parse_dates= ['date'], index_col= 'date')
 
-
-
 # Clean data
 df = df[(df['value'] >= df['value'].quantile(0.025)) & (df['value'] <= df['value'].quantile(0.975))]
-
-
 
 
 def draw_line_plot():
@@ -37,13 +33,17 @@ def draw_bar_plot():
     df['year'] = df.index.year
     
     
-    
     df_bar = df.groupby(['year', 'month'])['value'].mean()
     df_bar = df_bar.unstack()
 
     # Draw bar plot
     fig = df_bar.plot.bar(figsize=(15,10),ylabel='Average Page Views',xlabel='Years').figure
     plt.legend(['January','February','March','April','May','June','July','August','September','October','November','December'])
+    
+    
+
+
+
 
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
@@ -57,11 +57,22 @@ def draw_box_plot():
     df_box['month'] = [d.strftime('%b') for d in df_box.date]
 
     # Draw box plots (using Seaborn)
+    df_box['month_num'] = df_box['date'].dt.month
+    df_box = df_box.sort_values('month_num')
 
-    fig = sns.boxplot(x=df['year'],y=df['value']).figure
-    plt.title('Year-wise Box Plot (Trend)')
-    plt.xlabel('Year')
-    plt.ylabel('Page Views')
+    fig, axes= plt.subplots(nrows=1,ncols=2,figsize=(18,10))
+    axes[0] = sns.boxplot(x=df_box['year'], y=df_box['value'], ax=axes[0])
+    axes[1] = sns.boxplot(x=df_box['month'], y=df_box['value'], ax=axes[1])
+
+    axes[0].set_title('Year-wise Box Plot (Trend)')
+    axes[0].set_xlabel('Year')
+    axes[0].set_ylabel('Page Views')
+
+    axes[1].set_title('Month-wise Box Plot (Seasonality)')
+    axes[1].set_xlabel('Month')
+    axes[1].set_ylabel('Page Views')
+
+
 
 
 
